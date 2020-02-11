9077bd467c5278d2d205a47f4b30ad0f4a962d81
Marcelo <mansillamarcelo90@gmail.com>
2020-02-11 12:26:42 -0300

    uploading project

---
+++ Vet/src/main/java/Veterinaria/Vet/App.java
@@ -0,0 +1,13 @@
+package Veterinaria.Vet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
