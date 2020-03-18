0e1f664e94a0fa967c8ff894554a791bf6b313b4
Marcelo <mansillamarcelo90@gmail.com>
2020-03-18 19:23:22 -0300

    adding validation patternServiceHelper

--- Vet/src/main/java/ar/com/veterinaria/app/validatorPattern/ValidatorPatternServiceHelper.java
+++ Vet/src/main/java/ar/com/veterinaria/app/validatorPattern/ValidatorPatternServiceHelper.java
@@ -9,7 +9,7 @@ public class ValidatorPatternServiceHelper {
 	private static final String NUMBER_REGEX = "^[0-9]";
 	private static final Pattern NUMBER_PATTERN = Pattern.compile(NUMBER_REGEX);
 
-	private static final String NUMBER_LETTER_REGEX = "^[0-9]^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$";
+	private static final String NUMBER_LETTER_REGEX = "^[0-9]^[a-zA-Z]*$";
 	private static final Pattern NUMBER_LETTER_PATTERN = Pattern.compile(NUMBER_LETTER_REGEX);
 
 	public static Pattern getNamePattern() {
